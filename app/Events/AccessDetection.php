56ed706b165fc084c5add0055968bc168152f5a4
rito328 <ritolabo@gmail.com>
2019-08-12 12:16:46 +0900

    Added Event

---
+++ app/Events/AccessDetection.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class AccessDetection
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $param;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($value)
+    {
+        $this->param = $value;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
